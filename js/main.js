c65d0280b879f4a510d02785d009a33239cf9f45
a-vorozhbyt <dan_alla@ukr.net>
2020-07-20 16:01:45 +0300

    create: added swiper-slider in header-section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+   loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--prev',
+    prevEl: '.slider-button--prev',
+  },
+
+})
